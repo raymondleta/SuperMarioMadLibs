e2d0005ae3d0219ec78817b17b483c37ec0f8515
raymond gitonga <raytosh95@gmail.com>
2019-05-06 15:41:39 +0300

    create layout with madlibs forms

---
+++ app/src/main/java/com/tosh/madlibs/MadLibsActivity.java
@@ -0,0 +1,13 @@
+package com.tosh.madlibs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MadLibsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mad_libs);
+    }
+}
--- app/src/main/java/com/tosh/madlibs/MainActivity.java
+++ app/src/main/java/com/tosh/madlibs/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tosh.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MadLibsActivity.class);
+                startActivity(intent);
 
             }
         });
